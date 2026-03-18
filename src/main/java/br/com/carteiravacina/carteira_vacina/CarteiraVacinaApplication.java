e06aa7bcf5754f60184690cd7d1cd85cd4f3310d
Thais Machado <thaiscristianemachado@hotmail.com>
2026-03-18 19:42:56 -0300

    chore: cria estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/carteiravacina/carteira_vacina/CarteiraVacinaApplication.java
@@ -0,0 +1,13 @@
+package br.com.carteiravacina.carteira_vacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraVacinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraVacinaApplication.class, args);
+	}
+
+}
